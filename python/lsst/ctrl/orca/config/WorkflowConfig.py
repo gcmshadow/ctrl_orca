d8272c03fde2658b30ad5578b5ed4646967bbadc
Stephen Pietrowicz <srp@ncsa.illinois.edu>
2017-02-16 15:54:16 -0600

    Remove obsolete Config files

--- python/lsst/ctrl/orca/config/WorkflowConfig.py
+++ python/lsst/ctrl/orca/config/WorkflowConfig.py
@@ -1,17 +1,13 @@
 from __future__ import absolute_import
 import lsst.pex.config as pexConfig
 from . import CondorWorkflowConfig as condor
-from . import VanillaCondorWorkflowConfig as van
-from . import GenericWorkflowConfig as gen
 from . import FakeTypeMap as fake
 from . import DatabaseConfig as data
 from . import PlatformConfig as plat
 from . import MonitorConfig as mon
 from . import TaskConfig as task
 
-typemap = {"generic": gen.GenericWorkflowConfig,
-           "vanilla": van.VanillaCondorWorkflowConfig, 
-           "condor": condor.CondorWorkflowConfig,
+typemap = {"condor": condor.CondorWorkflowConfig,
            "pegasus": condor.CondorWorkflowConfig }
 
 #
@@ -24,9 +20,6 @@ class WorkflowConfig(pexConfig.Config):
 
     # platform configuration file
     platform = pexConfig.ConfigField("platform configuration file", plat.PlatformConfig)
-
-    # topic used for shutdown events
-    shutdownTopic = pexConfig.Field("topic used for shutdown events", str)
 
     # plugin type
     configurationType = pexConfig.Field("plugin type", str)
